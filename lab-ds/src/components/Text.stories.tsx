f3e0fc3bb9cd37edff289eab9e09e9bb4cb4d2b3
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2022-10-12 02:55:31 -0300

    Criando uma Propriedade Personalizada utilizando o Radix Slot

--- lab-ds/src/components/Text.stories.tsx
+++ lab-ds/src/components/Text.stories.tsx
@@ -5,25 +5,44 @@ export default {
     title: 'Components/Text',
     component: Text,
     args: {
-        children: 'Hello World',
+        children: 'Text MD',
         size: 'md',
     }
 } as Meta<TextProps>;
 
 export const Default: StoryObj<TextProps> = {
     args: {
+        children: 'Text Default',
+    }
+}
+
+export const Medium: StoryObj<TextProps> = {
+    args: {
         size: 'md',
+        children: 'Text Medium',
     }
 }
 
 export const Small: StoryObj<TextProps> = {
     args: {
         size: 'sm',
+        children: 'Text Small',
     }
 }
 
 export const Large: StoryObj<TextProps> = {
     args: {
         size: 'lg',
+        children: 'Text Large',
+    }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+    args: {
+        size: 'md',
+        asChild: true,
+        children: (
+            <p>Custom Text</p>
+        )
     }
 }
--- lab-ds/src/components/Text.tsx
+++ lab-ds/src/components/Text.tsx
@@ -1,11 +1,16 @@
+import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
+import { ReactNode } from 'react';
 
 export interface TextProps {
     size?: 'sm' | 'md' | 'lg';
-    children: String;
+    children: ReactNode;
+    asChild: boolean;
 }
 
-export function Text({ size = 'md', children }: TextProps) {
+export function Text({ size = 'md', children, asChild }: TextProps) {
+    const component = asChild ? Slot : 'span';
+
     return (
         <span
             className={clsx(
