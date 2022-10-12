1bfc3c937964d54a8daca1b8a9d59c8ba3fac70c
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2022-10-12 04:22:59 -0300

    Criando o componente button

---
+++ lab-ds/src/components/Button.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Button, ButtonProps } from './Button'
+
+export default {
+    title: 'Components/Button',
+    component: Button,
+    args: {
+        children: 'Button',
+    },
+    argTypes: {
+        asChild:{
+            table: {
+                disable: true,
+            },
+        }
+    }
+} as Meta<ButtonProps>;
+
+export const Default: StoryObj<ButtonProps> = {
+  
+}
