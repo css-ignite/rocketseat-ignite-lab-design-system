af598674fb4a0a3e087174862be7a4ea23853f79
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2022-10-12 02:09:25 -0300

    Importando os Tokens do Figma

--- lab-ds/src/App.tsx
+++ lab-ds/src/App.tsx
@@ -3,10 +3,10 @@ import './styles/global.css'
 export function App() {
   return (
     <>
-    <h1 className='font-bold text-5xl text-violet-500'>Olá mundo!</h1>
-    <button className='bg-violet-500 font-medium px-4 py-2 rounded hover:bg-violet-600'>
-      Enviar
-    </button>
+      <h1 className='font-bold text-5xl text-violet-500'>Olá mundo!</h1>
+      <button className='bg-cyan-500 font-medium px-4 py-2 rounded hover:bg-cyan-300'>
+        Enviar
+      </button>
     </>
   )
 }
--- lab-ds/tailwind.config.cjs
+++ lab-ds/tailwind.config.cjs
@@ -4,6 +4,23 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
+    fontSize: {
+      'xs': 14,
+      'sm': 16,
+      'md': 18,
+      'lg': 20,
+      'xl': 24,
+      '2xl': 32,
+    },
+    colors: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-700': '#7c7c8a',
+      'gray-200': '#c4c4cc',
+      'gray-100': '#e1e1e6',
+      'cyan-500': '#81d8f7',
+      'cyan-300': '#98e1fb',
+    },
     extend: {
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
